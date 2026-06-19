64bf70ab50bd264a97b87229bb330f229d3b29cd
Bhavyae <bhavyaeprasadpokhrel@gmail.com>
2026-06-19 09:55:32 +0530

    send mail on render URL3

--- server/controller/OTP.js
+++ server/controller/OTP.js
@@ -1,44 +1,66 @@
-const User=require("../models/User")
-const OtpGenerator=require("otp-generator")
-const OTP=require("../models/OTP")
+const User = require("../models/User");
+const OtpGenerator = require("otp-generator");
+const OTP = require("../models/OTP");
+const mailSender = require("../utils/mailSender");
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 
-exports.sendOTP=async(req,res)=>{
+exports.sendOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
 
-    try{
-        const {email}=req.body;
-    
-        if(!email){
-            return res.status(400).json({
-                success:false,
-                message:'Please Enter Email',
-            })
-        }
-    
-        const checkUserPresent=await User.findOne({email})
-    
-        if(checkUserPresent){
-            return res.status(401).json({
-                success:false,
-                message:'User Already Registered'
-    
-            })
-        }
-        var otp=OtpGenerator.generate(6,{
-            lowerCaseAlphabets:false,
-            upperCaseAlphabets:false,
-            specialChars:false
-        })
-         
-        const otpBody=await OTP.create({email,otp});
-        console.log(otpBody)
-        return res.status(200).json({
-            success:true,
-            otp,
-            message:'OTP Generated Successfully'
-        })
+    if (!email) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Enter Email",
+      });
     }
-    catch (error) {
-        console.log(error.message)
-        return res.status(500).json({ success: false, error: error.message })
+
+    const checkUserPresent = await User.findOne({ email });
+
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: "User Already Registered",
+      });
     }
-}
+
+    const otp = OtpGenerator.generate(6, {
+      lowerCaseAlphabets: false,
+      upperCaseAlphabets: false,
+      specialChars: false,
+    });
+
+    const otpBody = await OTP.create({ email, otp });
+    console.log("OTP document created:", otpBody._id);
+
+    try {
+      const mailResponse = await mailSender(
+        email,
+        "Verification Email",
+        emailTemplate(otp),
+      );
+      console.log("Mail response:", mailResponse.response);
+    } catch (mailError) {
+      await OTP.deleteOne({ _id: otpBody._id });
+      console.error("OTP mail error:", mailError.message);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to send OTP email",
+        error: mailError.message,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      otp,
+      message: "OTP Generated Successfully",
+    });
+  } catch (error) {
+    console.error("OTP controller error:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to generate OTP",
+      error: error.message,
+    });
+  }
+};
--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,41 +1,19 @@
-const mongoose=require('mongoose');
-const mailSender = require("../utils/mailSender");
-const emailTemplate = require("../mail/templates/emailVerificationTemplate");
+const mongoose = require('mongoose');
 
-const OTPSchema= mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
+const OTPSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
     },
-    createAt:{
-        type:Date,
-        default:Date.now,
-        expires:60*5
+    createAt: {
+        type: Date,
+        default: Date.now,
+        expires: 60 * 5,
     },
-    otp:{
-        type:String,
-        require:true,
-    }
-})
-
-const sendVerificationEmail = async (email, otp) => {
-    const mailResponse = await mailSender(email, 'Verification Email', emailTemplate(otp));
-    console.log('mail Response--->', mailResponse.response);
-}
-
-OTPSchema.pre("save", async function (next) {
-    if (this.isNew) {
-        try {
-            console.log('Preparing to send verification email:', { email: this.email, otp: this.otp });
-            await sendVerificationEmail(this.email, this.otp);
-            next();
-        } catch (error) {
-            console.log('FULL OTP ERROR:', error);
-            next(error);
-        }
-    } else {
-        next();
+    otp: {
+        type: String,
+        required: true,
     }
 });
 
-module.exports=mongoose.model("OTP",OTPSchema)
+module.exports = mongoose.model("OTP", OTPSchema);
